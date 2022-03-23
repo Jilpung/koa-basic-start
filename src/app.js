8f8db0df1b28e4d1e5e9c8b72598121f6c5c4541
Jilpung <86465970+Jilpung@users.noreply.github.com>
2022-03-24 08:07:38 +0900

    First commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+// import router from './routes';
+// import cors from 'cors';
+// import passport from 'passport';
+// import { Strategy as JwtStrategy } from 'passport-jwt';
+// import { jwtOptions, verifyUser } from './middlewares/passportJwt';
+
+const app = express();
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentals: true,
+// };
+
+// app.use(cors(corsOptions));
+app.use(express.json());
+// app.use(passport.initialize());
+// passport.use(new JwtStrategy(jwtOptions, verifyUser));
+
+// app.use(router);
+
+export default app;
