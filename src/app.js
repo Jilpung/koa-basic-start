dcad1954a18df5bf97e0fff9102b1a9b4f43ba53
Jilpung <86465970+Jilpung@users.noreply.github.com>
2022-04-01 17:01:42 +0900

    wip

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
-import express from 'express';
-// import router from './routes';
-// // import cors from 'cors';
-// import passport from 'passport';
-// import { Strategy as JwtStrategy } from 'passport-jwt';
-// import { jwtOptions, verifyUser } from './middlewares/passportJwt';
+import koa from 'koa';
+import Router from 'koa-router';
 
-const app = express();
+const app = new koa();
+const router = new Router();
+const api = require('./routes');
 
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-//   credentals: true,
-// };
+router.use('/routes', api.routes());
 
-// // app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(passport.initialize());
-// passport.use(new JwtStrategy(jwtOptions, verifyUser));
+app.use(router.routes()).use(router.allowedMethods());
 
-// app.use(router);
-
-export default app;
+app.listen(4000, () => {
+  console.log('port 4000');
+});
---
+++ src/routes/bookRouter.js
@@ -0,0 +1,10 @@
+import Router from 'koa-router';
+
+const api = new Router();
+
+api.get('/books', (ctx, next) => {
+  ctx.body = 'GET' + ctx.request.path;
+  next();
+});
+
+module.exports = api;
